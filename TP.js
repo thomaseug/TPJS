a90c215a9a5d6b567f9acd98dd458870ca370ac2
thomas EUGENIE <thomas.eugenie@protonmail.com>
2020-07-07 14:47:30 +0200

    first commit

---
+++ TP.js
@@ -0,0 +1,22 @@
+<doctype html>
+<html>
+<head>
+
+<head>
+<script>
+// Edit your script here
+var x = 1;
+
+
+while (x<66) {
+	if ( x%5==0 && x%7==0){
+	console.log(x);
+	}
+    x++;
+
+}
+
+
+</script>
+<!-- edit your html here -->
+<html>
